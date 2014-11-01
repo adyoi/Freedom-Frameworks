4738b79110c1830d39704abedabd2a4e3b583eb6
adyoi <adyoi@users.noreply.github.com>
2014-11-02 01:26:05 +0700

    Create config.php

---
+++ Version-1.0/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define ('url', dirname($_SERVER['SCRIPT_NAME']));
+define ('cls', 'Freedom_Frameworks');
+define ('eng', 'engine' . chr('047'));
+define ('ext', chr('046') . 'php');
+
+/* File_name : ./config/config.php */
+/* Freedom Frameworks Main_Config */
