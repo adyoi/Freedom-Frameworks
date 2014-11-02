478b7984f2935eb3431559f57f0addded49eac0d
adyoi <adyoi@users.noreply.github.com>
2014-11-02 12:04:56 +0700

    Create freedon_footer.php

---
+++ Version-1.0/views/freedon_footer.php
@@ -0,0 +1,14 @@
+</div>
+
+    </div>
+	
+	<div id="footer" style="text-align:center;padding:20px">
+		&copy; Freedom Frameworks 2014
+	</div>
+
+    <script src="<?php echo url; ?>/private/js/jquery.min.js"></script>
+    <script src="<?php echo url; ?>/private/js/bootstrap.min.js"></script>
+    <script src="<?php echo url; ?>/private/js/ie10-viewport-bug-workaround.js"></script>
+  
+</body>
+</html>
