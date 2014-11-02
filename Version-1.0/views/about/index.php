21de11e0f5a1b7a94419921224d6dba98609eb19
adyoi <adyoi@users.noreply.github.com>
2014-11-02 12:06:27 +0700

    Create index.php

---
+++ Version-1.0/views/about/index.php
@@ -0,0 +1,36 @@
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail" style="padding:25px 0 0;">
+      <img src="<?php echo url; ?>/private/images/History.png" alt="...">
+      <div class="caption">
+        <h3>History</h3>
+        <p>Version 1.0 Alpha on October 2014<br>Version 1.0 Beta on November 2014</p>
+		<p><a href="<?php echo url; ?>/index/history" class="btn btn-primary" role="button"><span class="glyphicon glyphicon-pencil"></span> Change Logs</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail" style="padding:25px 0 0;">
+      <img src="<?php echo url; ?>/private/images/FreedomFrameworks.png" alt="...">
+      <div class="caption">
+        <h3>Freedom Frameworks</h3>
+        <p>Freedom Frameworks is Simply and Powerful<br>This Frameworks built in Native PHP</p>
+		<p><a href="https://github.com/adyoi/FreedomFrameworks" class="btn btn-success" role="button" target="_blank"><span class="glyphicon glyphicon-search"></span> View on Github</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail" style="padding:25px 0 0;">
+      <img src="<?php echo url; ?>/private/images/AboutMe.png" alt="...">
+      <div class="caption">
+        <h3>About Me</h3>
+        <p>Adi Apriyanto<br>Born at Jakarta, Indonesia</p>
+		<p><a href="https://twitter.com/adyoi" class="btn btn-success" role="button" target="_blank"><span class="glyphicon glyphicon-user"></span> Twitter</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div clas="row">
+	<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a><br /><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Freedom Frameworks</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="website" property="cc:attributionName" rel="cc:attributionURL">Adi Apriyanto</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International License</a>.
+</div>
