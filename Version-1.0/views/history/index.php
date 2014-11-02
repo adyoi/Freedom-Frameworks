88a5feaef70e09da4544c85d477012011d8beb51
adyoi <adyoi@users.noreply.github.com>
2014-11-02 12:09:07 +0700

    Create index.php

---
+++ Version-1.0/views/history/index.php
@@ -0,0 +1,13 @@
+<ul class="list-group">
+  <li class="list-group-item active">Version 1.0 Beta</li>
+  <li class="list-group-item">1. With PDO Database Drivers</li>
+  <li class="list-group-item">2. Bootstrap Default Viewer</li>
+  <li class="list-group-item">3. Array Parameter Controller</li>
+  <li class="list-group-item">4. Optimization Script Code</li>
+</ul>
+
+<ul class="list-group">
+  <li class="list-group-item active">Version 1.0 Alpha</li>
+  <li class="list-group-item">1. With PDO Database Drivers</li>
+  <li class="list-group-item">2. Bootstrap Default Viewer</li>
+</ul>
