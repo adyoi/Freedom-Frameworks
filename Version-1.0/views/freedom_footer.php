bea528888c2679ff2f423a5d226a64f81f3f9f8a
adyoi <adyoix@gmail.com>
2014-11-03 09:54:37 +0700

    Release
    Version 1.0 Beta

---
+++ Version-1.0/views/freedom_footer.php
@@ -0,0 +1,15 @@
+</div>
+
+    </div>
+	
+	<div id="footer" style="text-align:center;padding:20px">
+		&copy; Freedom Frameworks 2014
+	</div>
+
+    <script src="<?php echo url; ?>/private/js/jquery.min.js"></script>
+    <script src="<?php echo url; ?>/private/js/bootstrap.min.js"></script>
+    <script src="<?php echo url; ?>/private/js/ie10-viewport-bug-workaround.js"></script>
+	<script src="<?php echo url; ?>/private/js/custom.js"></script>
+  
+</body>
+</html>
